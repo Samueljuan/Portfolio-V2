e8919934b493db42def1e10dda4baa378ffc7d2f
Samuel Juan <samueljuan0619@gmail.com>
2025-12-28 18:09:29 +0700

    phase 6.5

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,10 +64,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
       { url: '/favicon.png', type: 'image/png' },
-      { url: '/favicon-samuel.ico' },
-      { url: '/favicon-samuel.svg', type: 'image/svg+xml' },
-      { url: '/favicon-samuel.png', type: 'image/png' },
     ],
     apple: '/icons/icon-192.png',
   },
