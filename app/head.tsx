b86480dfc90b717c9c7c5b0d9ce7f8ff0435fbeb
Samuel Juan <samueljuan0619@gmail.com>
2025-12-28 19:19:18 +0700

    phase 6.6

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+        <>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+            <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+            <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32" />
+        </>
+    )
+}
